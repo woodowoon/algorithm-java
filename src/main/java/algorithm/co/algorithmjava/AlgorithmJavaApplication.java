d1bfc5989aa74781b6e7b89892ef8da39bd6b37d
Dowoon <woodw920@gmail.com>
2023-11-21 23:05:35 +0900

    groupId 변경

--- src/main/java/algorithm/co/algorithmjava/AlgorithmJavaApplication.java
+++ src/main/java/algorithm/co/algorithmjava/AlgorithmJavaApplication.java
@@ -1,4 +1,4 @@
-package com.example.algorithmjava;
+package algorithm.co.algorithmjava;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/algorithm/co/algorithmjava/AlgorithmJavaApplicationTests.java
+++ src/test/java/algorithm/co/algorithmjava/AlgorithmJavaApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.algorithmjava;
+package algorithm.co.algorithmjava;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
