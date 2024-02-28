be5760899968e1d24878f87af0bc391374b18787
dowoon <woodw920@gmail.com>
2024-02-28 09:21:51 +0900

    회전초밥 이해 주석 추가

--- src/main/java/algorithm/co/algorithmjava/code/baekJoon/SushiRotation.java
+++ src/main/java/algorithm/co/algorithmjava/code/baekJoon/SushiRotation.java
@@ -38,18 +38,27 @@ public class SushiRotation {
 
         maxVariety = kindCount;
 
+        System.out.println("sushiCount1 = " + sushiCount);
+
         // 초밥 창구를 한 칸씩 이동하며 최대 가짓수 계산
+        // i 가 0 부터 시작하는 이유는 회전하기때문에 첫번째와 마지막초밥이 붙어있다.
         for (int i = 0; i < n; i++) {
             // 창구에서 나간 초밥 처리
             int removedSushi = belt.get(i); // 벨트에서 나가는 초밥
-            sushiCount.put(removedSushi, sushiCount.get(removedSushi) - 1);
+            System.out.println("removedSushi = " + removedSushi);
+            sushiCount.put(removedSushi, sushiCount.get(removedSushi) - 1); // 첫번째 개수를 하나 줄이고
 
-            if (sushiCount.get(removedSushi) == 0) {
-                sushiCount.remove(removedSushi);
+            System.out.println("removedSushi1 = " + removedSushi);
+            System.out.println("sushiCount2 = " + sushiCount);
+
+            if (sushiCount.get(removedSushi) == 0) { // 만약 개수를 줄인게 0이라면
+                sushiCount.remove(removedSushi); // 해당 초밥을 삭제해.
             }
 
+            System.out.println("sushiCount3 = " + sushiCount);
+
             // 새로운 초밥 추가
-            int newSushi = belt.get((i + k) % n); // 새로운 초밥
+            int newSushi = belt.get((i + k) % n); // 새로운 초밥 (회전하는 회전초밥이기때문에 %n 을 추가했다.)
             sushiCount.put(newSushi, sushiCount.getOrDefault(newSushi, 0) + 1);
 
             // 새로운 가짓수 계산
