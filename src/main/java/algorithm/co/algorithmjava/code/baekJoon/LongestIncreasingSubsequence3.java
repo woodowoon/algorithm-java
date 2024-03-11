4a62ba3e5fad798780e55ea939a52cbcca884609
dowoon <woodw920@gmail.com>
2024-03-11 09:09:14 +0900

    [12738] 가장 긴 증가하는 부분 수열3

---
+++ src/main/java/algorithm/co/algorithmjava/code/baekJoon/LongestIncreasingSubsequence3.java
@@ -0,0 +1,54 @@
+package algorithm.co.algorithmjava.code.baekJoon;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+// 가장 긴 증가하는 부분 수열3 12738번
+public class LongestIncreasingSubsequence3 {
+
+    public static void input() {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        List<Integer> numbers = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            numbers.add(scanner.nextInt());
+        }
+
+        scanner.close();
+
+        logic(numbers);
+    }
+
+    private static void logic(List<Integer> numbers) {
+        List<Integer> newList = new ArrayList<>();
+
+        for (Integer number : numbers) {
+            if (newList.isEmpty() || number > newList.get(newList.size() - 1)) {
+                newList.add(number);
+            } else {
+                int i = 0;
+                int j = newList.size();
+
+                while (i < j) {
+                    int mid = (i + j) / 2;
+
+                    if (number > newList.get(mid)) {
+                        i = mid + 1;
+                    } else {
+                        j = mid;
+                    }
+                }
+
+                newList.set(i, number);
+            }
+
+        }
+
+        System.out.println(newList.size());
+
+    }
+
+}
